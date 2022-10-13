5b2fab57767694e6b4963ef0ef73c94451fe3b5c
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2022-10-13 13:47:19 +0900

    add normal evaluation (#29)

--- evaluate/src/normal_evaluator.py
+++ evaluate/src/normal_evaluator.py
@@ -28,14 +28,7 @@ class NormalEvaluator(BaseEvaluator):
         hydra_overrides: List[str] = [],
     ) -> None:
         super().__init__(
-            model,
-            model_name,
-            test_dataset,
-            input_parameter_names,
-            output_parameter_names,
-            downstream_directory,
-            observation_point_file_path,
-            hydra_overrides,
+            model, model_name, test_dataset, input_parameter_names, output_parameter_names, downstream_directory, observation_point_file_path, hydra_overrides,
         )
 
     def run(self) -> Dict[str, float]:
@@ -68,18 +61,10 @@ class NormalEvaluator(BaseEvaluator):
             rescaled_pred_tensor = all_rescaled_pred_tensors[0, 0, time_step, ...]
             label_df = self.test_dataset[test_case_name]["label_df"][time_step]
             self.add_result_df_from_pred_tensor(
-                test_case_name,
-                time_step,
-                rescaled_pred_tensor,
-                label_df,
-                output_param_name,
+                test_case_name, time_step, rescaled_pred_tensor, label_df, output_param_name,
             )
             self.add_metrics_df_from_pred_tensor(
-                test_case_name,
-                time_step,
-                rescaled_pred_tensor,
-                label_df,
-                output_param_name,
+                test_case_name, time_step, rescaled_pred_tensor, label_df, output_param_name,
             )
 
         save_dir_path = os.path.join(self.downstream_direcotry, self.model_name, "normal_evaluation", test_case_name)
